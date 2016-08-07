7cd58fcc94ec131ae5d36fbe137b38873bc9acac
Ruyu You <727809166@qq.com>
2016-08-07 22:21:41 +0800

    add class AddTodoItem and Todolist

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 const App = React.createClass({
+    getInitialState: function () {
+        return {
+            todoTitle: 'todos',
+            todoItems: []
+        }
+    },
+
+    addItem: function () {
+
+    },
+
     render: function () {
         return <div>
-            Hello
+            <h1 className="text-center text-danger">{this.state.todoTitle}</h1>
+            <AddTodoItem />
+            <Todolist />
         </div>
     }
 });
+
+const AddTodoItem = React.createClass({
+    render: function () {
+        return <div className={this.props.className}>AddTodoItem
+        </div>
+    }
+});
+
+const Todolist = React.createClass({
+    render: function () {
+        return <div className={this.props.className}>Todolist
+        </div>
+    }
+})
 
 ReactDOM.render(<App/>, document.getElementById('content'));
